4cecef2226c08d687c909c1a294f1806230d64c1
Marcus Ottosson <konstruktion@gmail.com>
2014-08-06 16:40:33 +0100

    More validators for feature test

---
+++ publish/validators/blank2_validator.py
@@ -0,0 +1,20 @@
+"""Test for the existence of muted channels
+
+Attributes:
+    __families__: In which families this test applies.
+    __version__: Current version
+    __hosts__: Supported hosts
+
+"""
+
+__families__ = ['pointcache']
+__version__ = (0, 1)
+__hosts__ = ['maya']
+
+
+def process(context):
+    pass
+
+
+def fix(context):
+    pass
