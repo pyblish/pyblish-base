653425a195072e9725b44633d86acd813eb0c80a
Marcus Ottosson <konstruktion@gmail.com>
2014-08-28 13:34:46 +0200

    Including contributor notice as authors

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
     version=version,
     description='quality assurance for content',
     long_description=open('README.txt').read(),
-    author='Marcus Ottosson',
+    author='Abstract Factort and Contributors',
     author_email='marcus@abstractfactory.com',
     url='https://github.com/abstractfactory/pyblish',
     license='LGPL',
