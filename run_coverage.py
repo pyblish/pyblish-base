aa8d916817f4669b04f81edb4c6310139019eedc
Marcus Ottosson <konstruktion@gmail.com>
2014-09-11 20:19:53 +0200

    Tweaking travis

--- run_coverage.py
+++ run_coverage.py
@@ -8,7 +8,7 @@ sys.path.insert(0, path)
 from pyblish.vendor import nose
 
 argv = sys.argv[:]
-argv.extend(['--exclude=vendor', '--cover-package=pyblish',
-             '--where=.', '--with-doctest', '--with-coverage',
+argv.extend(['--exclude=vendor',
+             '--with-doctest', '--with-coverage',
              '--cover-html', '--cover-erase'])
 nose.main(argv=argv)
