348575a79c675a7935652fc960d22af06740e05b
Marcus Ottosson <konstruktion@gmail.com>
2014-09-20 15:15:21 +0100

    Update plugins to API

--- pyblish/plugins/select_current_user.py
+++ pyblish/plugins/select_current_user.py
@@ -1,14 +1,11 @@
-"""Inject the currently logged on user into the Context"""
 
 import getpass
-
-import pyblish.backend.lib
-import pyblish.backend.plugin
+import pyblish
 
 
-@pyblish.backend.lib.log
-class SelectCurrentUser(pyblish.backend.plugin.Selector):
-    """Append the currently logged on user"""
+@pyblish.log
+class SelectCurrentUser(pyblish.Selector):
+    """Inject the currently logged on user into the Context"""
 
     hosts = ['*']
     version = (0, 1, 0)
