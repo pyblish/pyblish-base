3d0dd14072e10299c05c98825eee6b5cae3fc31f
Marcus Ottosson <konstruktion@gmail.com>
2014-09-30 10:56:18 +0100

    Improving CLI with proper program name in Console, from "__main__.py" to "pyblish"

--- pyblish/__main__.py
+++ pyblish/__main__.py
@@ -7,4 +7,4 @@ This makes the Pyblish package into an executable, via cli.py
 import pyblish.cli
 
 if __name__ == '__main__':
-    pyblish.cli.main(obj={})
+    pyblish.cli.main(obj={}, prog_name="pyblish")
