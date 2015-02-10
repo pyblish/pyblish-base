e62e6de6800945e61475ce4c3b67177ea8c10aaa
Marcus Ottosson <konstruktion@gmail.com>
2015-02-10 17:11:50 +0000

    Temporarily removing command-line interface

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
-with open("README.txt") as f:
-    readme = f.read()
-
-
 import os
 import imp
+
+
+with open("README.txt") as f:
+    readme = f.read()
 
 version_file = os.path.abspath("pyblish/version.py")
 version_mod = imp.load_source("version", version_file)
@@ -69,8 +69,5 @@ setup(
     package_data={
         "pyblish": ["plugins/*.py", "*.yaml", "vendor/nose/*.txt"],
         "pyblish.tests": tests_package_data,
-    },
-    entry_points={
-        "console_scripts": ["pyblish = pyblish.cli:main"]
     },
 )
