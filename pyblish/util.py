22830015e4f482468aa1cc729fb3505ebd73a43f
Marcus Ottosson <konstruktion@gmail.com>
2015-07-11 11:53:54 +0100

    Convenience functions now respect negative offsets on order

--- pyblish/util.py
+++ pyblish/util.py
@@ -81,22 +81,22 @@ def publish(context=None, plugins=None, **kwargs):
 
 def select(*args, **kwargs):
     """Convenience function for selection"""
-    return _convenience(1, *args, **kwargs)
+    return _convenience(1.5, *args, **kwargs)
 
 
 def validate(*args, **kwargs):
     """Convenience function for validation"""
-    return _convenience(2, *args, **kwargs)
+    return _convenience(2.5, *args, **kwargs)
 
 
 def extract(*args, **kwargs):
     """Convenience function for extraction"""
-    return _convenience(3, *args, **kwargs)
+    return _convenience(3.5, *args, **kwargs)
 
 
 def conform(*args, **kwargs):
     """Convenience function for conform"""
-    return _convenience(4, *args, **kwargs)
+    return _convenience(4.5, *args, **kwargs)
 
 
 collect = select
