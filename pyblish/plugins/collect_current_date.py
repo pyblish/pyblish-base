37c0d4bb1263a2219cce8b92f2a2ce534267f068
Marcus Ottosson <konstruktion@gmail.com>
2016-02-27 12:19:14 +0000

    Add labels to default plug-ins

--- pyblish/plugins/collect_current_date.py
+++ pyblish/plugins/collect_current_date.py
@@ -7,6 +7,7 @@ class CollectCurrentDate(pyblish.api.ContextPlugin):
     """Inject the current time into the Context"""
 
     order = pyblish.api.CollectorOrder
+    label = "Current date"
 
     def process(self, context):
         context.data['date'] = pyblish.lib.time()
--- pyblish/plugins/collect_current_working_directory.py
+++ pyblish/plugins/collect_current_working_directory.py
@@ -7,6 +7,7 @@ class CollectCurrentWorkingDirectory(pyblish.api.ContextPlugin):
     """Inject the current working directory into Context"""
 
     order = pyblish.api.CollectorOrder
+    label = "Current working directory"
 
     def process(self, context):
         context.data['cwd'] = os.getcwd()
