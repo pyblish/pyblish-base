901d4ba6b33d46c5c2f0c6adb685aa28dec608ca
Toke Jepsen <tokejepsen@gmail.com>
2017-09-04 12:41:07 +0200

    Validator for publish data member type.

---
+++ pyblish/plugins/validate_publish_data_type.py
@@ -0,0 +1,13 @@
+import pyblish.api
+
+
+class ValidatePublishDataType(pyblish.api.ContextPlugin):
+    """Validates the "publish" data members type to be boolean."""
+
+    order = pyblish.api.ValidatorOrder
+    label = "Publish Data Type"
+
+    def process(self, context):
+
+        for instance in context:
+            assert isinstance(instance.data["publish"], bool)
