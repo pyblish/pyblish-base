c5fa01d3d14cffad69d6f46ed97b448d64690b5d
aardschok <wikoreman@gmail.com>
2018-02-20 11:54:45 +0100

    fixed test function for targets

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -238,51 +238,40 @@ if __name__ == '__main__':
 
 @with_setup(lib.setup, lib.teardown)
 def test_set_targets():
+    """Setting targets works"""
 
-    with tempfile.NamedTemporaryFile(dir=self.tempdir,
-                                     delete=False,
-                                     suffix=".py") as f:
-        module_name = os.path.basename(f.name)[:-3]
-        f.write(b"""\
-    from pyblish import api
-    
-    print("A")
-    def show():
-        registered = api.registered_targets()
-        print(registered)
-
-    if __name__ == '__main__':
-        show()
-    """)
-
-    os.environ["PYBLISH_HOSTS"] = "shell"
     pythonpath = os.pathsep.join([
         self.tempdir,
         os.environ.get("PYTHONPATH", "")
     ])
 
-    # Create plugin with target
     count = {"#": 0}
 
-    class Collector(pyblish.api.ContextPlugin):
+    class CollectorOne(pyblish.api.ContextPlugin):
         order = pyblish.api.CollectorOrder
         targets = ["imagesequence"]
 
         def process(self, context):
-            self.log.warning("Running")
+            self.log.warning("Running {}".format(self.targets))
             count["#"] += 1
             context.create_instance("MyInstance")
 
-    pyblish.api.register_plugin(Collector)
+    class CollectorTwo(pyblish.api.ContextPlugin):
+        order = pyblish.api.CollectorOrder
+        targets = ["model"]
+
+        def process(self, context):
+            self.log.warning("Running {}".format(self.targets))
+            count["#"] += 2
+            context.create_instance("MyInstance")
+
+    pyblish.api.register_plugin(CollectorOne)
+    pyblish.api.register_plugin(CollectorTwo)
 
     runner = CliRunner()
     result = runner.invoke(pyblish.cli.main,
-                           ["publish",
-                            "--targets",
-                            "imagesequence",
-                            module_name],
+                           ["publish", "--targets", "imagesequence"],
                            env={"PYTHONPATH": pythonpath})
 
-    assert_equals(result.output.splitlines()[-1].rstrip(),
-                  "imagesequence")
-    assert_equals(result.exit_code, 0)
+    print(result.output)
+    assert count["#"] == 1, count
