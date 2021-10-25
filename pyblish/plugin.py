0baed4f2b97e0f94b56f52215b4b4e33fb9b8915
hannes.delbeke <hannesdelbeke@gmail.com>
2021-10-25 17:45:56 +0100

    support- new path type in-plugin-registration by converting to string if not string

--- pyblish/plugin.py
+++ pyblish/plugin.py
@@ -1045,6 +1045,8 @@ def register_plugin_path(path):
         Actual path added, including any post-processing
 
     """
+    if path and not isinstance(path, str):
+        path = str(path)
 
     normpath = os.path.normpath(path)
     if normpath in _registered_paths:
