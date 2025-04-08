8428cc52a3ffe1edba8821c90481096c68b0de6c
Michael Dinitz <michaeldinitz@yahoo.de>
2025-04-08 18:19:44 +0200

    reworked plugin to still use six exec but complie the code before

--- pyblish/plugin.py
+++ pyblish/plugin.py
@@ -21,7 +21,6 @@ import warnings
 import contextlib
 import uuid
 
-
 # Local library
 from . import (
     __version__,
@@ -39,10 +38,8 @@ from . import lib
 from .vendor import iscompatible, six
 
 if six.PY2:
-    import imp
     get_arg_spec = inspect.getargspec
 else:
-    import importlib.machinery
     get_arg_spec = inspect.getfullargspec
 
 log = logging.getLogger("pyblish.plugin")
@@ -1360,18 +1357,13 @@ def discover(type=None, regex=None, paths=None):
                 log.debug("Skipped: \"%s\",\"%s\", not end in .py", mod_name, mod_ext)
                 continue
 
-            
-            
+            module = types.ModuleType(mod_name)
+            module.__file__ = abspath
+
             try:
-                if six.PY2:
-                    module = imp.load_module(mod_name)
-                    module.__file__ = abspath
-                else:
-                    loader = importlib.machinery.SourceFileLoader(mod_name,abspath)
-                    module = types.ModuleType(mod_name)
-                    module.__file__ = abspath
-                    # exec might cause issues with python < 3.4
-                    loader.exec_module(module)
+                with open(abspath, "rb") as f:
+                    code = compile(f.read(),abspath,"exec")
+                    six.exec_(code, module.__dict__)
 
                 # Store reference to original module, to avoid
                 # garbage collection from collecting it's global
